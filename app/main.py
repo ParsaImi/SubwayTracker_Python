29e4b6f5f190e658dcb35180c3d8f791f71e695f
parsa <parsaemani17@gmail.com>
2024-04-13 22:45:01 +0330

    refactoring

--- app/main.py
+++ app/main.py
@@ -53,7 +53,6 @@ def start():
     globsrc = mysrc["state"]
     myglobsrc = globsrc
     mydata = hasPath(mysrc["state"] , mydst["state"])
-    print("ooooooooooooooooooooo" , mydata)
     mymen = numToName(mydata)
     return render_template("start.html" , mydata=mymen)
     # return render_template('index.html', items=perfect)
@@ -67,22 +66,14 @@ def start():
 
 
 def numToName(numlist):
+    lineColors = {1 : "redline" , 2 : "blueline" , 3 : "skyblueline" , 4 :"yellowline" , 5 : "greenline" , 6 : "pinkline", 7 : "purpleline"}
     items = []
     for i in numlist :
         mydb = client["test_database"]["posts"]
         myquery = { "state" : i }
         myitem = mydb.find_one(myquery)
-        print(myitem)
-        items.append(myitem["name"])
-        # !!!!!!!!!!!! IMPORTANT Print!!!!!!!!!!!!!!
-        # !!!!!!!!!!!! IMPORTANT Print!!!!!!!!!!!!!!
-        # !!!!!!!!!!!! IMPORTANT Print!!!!!!!!!!!!!!
-        # !!!!!!!!!!!! IMPORTANT Print!!!!!!!!!!!!!!
-        # !!!!!!!!!!!! IMPORTANT Print!!!!!!!!!!!!!!
-        # !!!!!!!!!!!! IMPORTANT Print!!!!!!!!!!!!!!
-        # !!!!!!!!!!!! IMPORTANT Print!!!!!!!!!!!!!!
-        # !!!!!!!!!!!! IMPORTANT Print!!!!!!!!!!!!!!
-         #print(items , "0000000000000000000000000033333333333333333333333333")
+        myTuple = (myitem["name"] , lineColors[myitem["line"]])
+        items.append(myTuple)
     return items
 
 
@@ -91,15 +82,12 @@ def hasPath(src , dst):
     latone = None
     globsrc = src
     queue = [[src , 0 , src]]
-    print(direction)
+   
     arrayLen = len(queue)
     while arrayLen > 0 :
         [src , distance , saheb] = queue.pop()
         direction.append([src , distance , saheb])
-        print(direction , "llllllllllllllllllllllllllllllllllllllllllllll")
-        print(queue)
         if(src == dst) :
-            print(direction)
             fortest = roadfinder(src , saheb , globsrc)
             latone = fortest
             return latone
@@ -110,21 +98,17 @@ def hasPath(src , dst):
         distance += 1
         for neighbor in neighbors :
             if neighbor not in mySet :
-                print( neighbor ,"checked !")
                 mySet.add(neighbor)
                 mySet.add(src)
                 queue.insert(0 , [neighbor , distance , src])
-                print(queue , "myQwas")
-    print(latone , "5555555555555555555555555555555555555555555555555555555555555555555555555")
     queue = []
     mySet = {}
     return latone  
                 
 def roadfinder(src , saheb , globsrc ) :
-    mypath.append(src)
+    mypath.insert(0,src)
     print(direction , "this is direction")
     if src != globsrc :
-        #print(src , globsrc , "tjosssssssssssssssssssssssssssssssssssssssss")
         for i in direction : 
             if i[0] == saheb :
                 newSrc = i[0]
@@ -136,15 +120,6 @@ def roadfinder(src , saheb , globsrc ) :
 
 
 
-            # newSrc = i[0]
-            # newSaheb = i[1]
-            # print(newSaheb , newSrc)
-            # myFinal.append(newSrc)
-    # if newSrc != newSaheb :
-    #     roadfinder(newSrc , newSaheb)
-    # else :
-    #     print(myFinal)
-    #     return
         
 
 
